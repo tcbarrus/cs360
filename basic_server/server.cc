de88769c298afc104b0db9cfdb3a2ec8920db3db
Trevor Barrus <tcbarrus@gmail.com>
2016-01-29 00:55:39 -0700

    Removed signal catching

--- basic_server/server.cc
+++ basic_server/server.cc
@@ -77,14 +77,14 @@ int main(int argc, char* argv[])
     int rc1, rc2;
 
     // First set up the signal handler
-    struct sigaction sigold, signew;
-    signew.sa_handler=handler;
-    sigemptyset(&signew.sa_mask);
-    sigaddset(&signew.sa_mask,SIGINT);
-    signew.sa_flags = SA_RESTART;
-    sigaction(SIGINT,&signew,&sigold);
-    sigaction(SIGHUP,&signew,&sigold);
-    sigaction(SIGPIPE,&signew,&sigold);
+    // struct sigaction sigold, signew;
+    // signew.sa_handler=handler;
+    // sigemptyset(&signew.sa_mask);
+    // sigaddset(&signew.sa_mask,SIGINT);
+    // signew.sa_flags = SA_RESTART;
+    // sigaction(SIGINT,&signew,&sigold);
+    // sigaction(SIGHUP,&signew,&sigold);
+    // sigaction(SIGPIPE,&signew,&sigold);
 
     if(argc < 3)
       {
