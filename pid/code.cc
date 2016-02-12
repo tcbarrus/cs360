a67c7394958562e1fecf228b8ab0e349b93a1a85
tcbarrus <trevor.barrus@byu.edu>
2016-02-12 18:21:11 +0000

    more code 12 Feb

--- pid/code.cc
+++ pid/code.cc
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <cstdlib>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h> 
 main(){
 	int pid = fork();
 	std::cout << "PID is "<<pid<<std::endl;
 	if(pid == 0){
-
+		std::cout<< "about to exec"<<std::endl;
+		execl("/bin/ls","/bin/ls",(char*)0);
 	}else{
 		int status;
-		wait($status);
+		wait(&status);
+		std::cout <<"after wait "<<std::endl;
 	}
+	exit(4);
 }
