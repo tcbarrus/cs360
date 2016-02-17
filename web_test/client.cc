d15f66ebe798328676a0b1e5b2bee8fe7132799d
Trevor Barrus <tcbarrus@gmail.com>
2016-02-16 19:00:40 -0700

    customize epoll code

--- web_test/client.cc
+++ web_test/client.cc
@@ -13,13 +13,36 @@
 #define SOCKET_ERROR        -1
 #define BUFFER_SIZE         10000
 #define HOST_NAME_SIZE      255
-#define NSOCKETS 10
 
 int  main(int argc, char* argv[])
 {
-    int hSocket[NSOCKETS];                 /* handle to socket */
-    struct timeval oldtime[NSOCKETS];
-    struct timeval newtime[NSOCKETS];
+    int count;
+    bool debug = false;
+
+    while ((option = getopt(argc,argv,"d")) != -1) {
+        switch (option) {
+            case 'd':
+                debug = true;
+                break;
+            default:
+                printf("\nUsage: [-d request and headers] [-c count] client host-name host-port url\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+    if(argv[optind] == NULL || argv[optind+1] == NULL || argv[optind+2] == NULL || argv[optind+3] == NULL){
+        printf("\nUsage: [-d request and headers] [-c count] client host-name host-port url\n");
+        return 0;
+    }
+    else{
+        strcpy(strHostName,argv[optind]);
+        nHostPort=atoi(argv[optind+1]);
+        url=argv[optind+2];
+        count=atoi(argv[optind+3]);
+    }
+
+    int hSocket[count];                 /* handle to socket */
+    struct timeval oldtime[count];
+    struct timeval newtime[count];
     struct hostent* pHostInfo;   /* holds info about a machine */
     struct sockaddr_in Address;  /* Internet socket address stuct */
     long nHostAddress;
@@ -28,20 +51,9 @@ int  main(int argc, char* argv[])
     char strHostName[HOST_NAME_SIZE];
     int nHostPort;
 
-    if(argc < 3)
-      {
-        printf("\nUsage: client host-name host-port\n");
-        return 0;
-      }
-    else
-      {
-        strcpy(strHostName,argv[1]);
-        nHostPort=atoi(argv[2]);
-      }
-
     printf("\nMaking a socket");
     /* make a socket */
-    for(int i = 0; i < NSOCKETS; i++) {
+    for(int i = 0; i < count; i++) {
         hSocket[i]=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
         if(hSocket[i] == SOCKET_ERROR)
@@ -63,7 +75,7 @@ int  main(int argc, char* argv[])
 
     int epollFD = epoll_create(1);
     // Send the requests and set up the epoll data
-    for(int i = 0; i < NSOCKETS; i++) {
+    for(int i = 0; i < count; i++) {
         /* connect to host */
         if(connect(hSocket[i],(struct sockaddr*)&Address,sizeof(Address)) 
            == SOCKET_ERROR)
@@ -71,9 +83,11 @@ int  main(int argc, char* argv[])
             printf("\nCould not connect to host\n");
             return 0;
         }
-        char request[] = "GET /test.html HTTP/1.0\r\n\r\n";
+        // char request[] = "GET /test.html HTTP/1.0\r\n\r\n";
+        char *request = (char *)malloc(MAXMSG);
+        sprintf(request, "GET %s HTTP/1.1\r\nHost: %s:%d\r\n\r\n", url.c_str(), strHostName, nHostPort);
 
-            write(hSocket[i],request,strlen(request));
+        write(hSocket[i],request,strlen(request));
         struct epoll_event event;
         event.data.fd = hSocket[i];
         event.events = EPOLLIN;
@@ -82,7 +96,7 @@ int  main(int argc, char* argv[])
             perror ("epoll_ctl");
         gettimeofday(&oldtime[i], NULL);
     }
-    for(int i = 0; i < NSOCKETS; i++) {
+    for(int i = 0; i < count; i++) {
         struct epoll_event event;
         int rval = epoll_wait(epollFD,&event,1,-1);
         if(rval < 0)
@@ -90,7 +104,8 @@ int  main(int argc, char* argv[])
         read(event.data.fd,pBuffer,BUFFER_SIZE);
         gettimeofday(&newtime[i], NULL);
         double usec = (newtime[i].tv_sec - oldtime[i].tv_sec)*(double)1000000+(newtime[i].tv_usec-oldtime[i].tv_usec);
-        std::cout << "Time "<<usec/1000000<<std::endl;
+        if(debug)
+            std::cout << "Time "<<usec/1000000<<std::endl;
         printf("got from %d\n",event.data.fd);
 
         printf("\nClosing socket\n");
