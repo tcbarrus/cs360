aa12feb0f6ca81e4a6f18580586496646465400c
Trevor Barrus <tcbarrus@gmail.com>
2016-03-07 08:48:52 -0700

    finish getcity protocol

--- node_server/routes/index.js
+++ node_server/routes/index.js
@@ -4,17 +4,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.sendFile('weather.html', {root: 'public'});
 });
 router.get('/getcity', function(req,res,next){
-	console.log("in getcity route");
 	fs.readFile(__dirname + '/cities.dat.txt', function(err,data){
 		if(err) throw err;
 		var cities = data.toString().split("\n");
+		var myRe = new RegExp("^" + req.query.q);
+		console.log(meRe);
+		var json = [];
 		for(var i = 0; i < cities.length; i++){
-			console.log(cities[i]);
+			var result = cities[i].search(myRe);
+			if(result != -1){
+				// console.log(cities[i]);
+				json.push({city:cities[i]});
+			}
 		}
+		res.status(200).json(json);
 	});
 });
 
