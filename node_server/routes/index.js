b9f23a326e2a01581971fc4509ea9c04e2b9ad32
Trevor Barrus <tcbarrus@gmail.com>
2016-03-07 08:30:59 -0700

    getcity service

--- node_server/routes/index.js
+++ node_server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.sendFile('weather.html', {root: 'public'});
+});
+router.get('/getcity', function(req,res,next){
+	console.log("in getcity route");
 });
 
 module.exports = router;
