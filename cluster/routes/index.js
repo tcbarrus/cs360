129bf284aab54f73b0ece045966f7817f20064ee
tcbarrus <trevor.barrus@byu.edu>
2016-04-13 05:33:56 +0000

    Added cluster lab

---
+++ cluster/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log("ID "+process.pid);
+  res.render('index', { title: 'Cluster', pid:process.pid });
+});
+
+router.get('/pid', function(req, res, next) {
+console.log("Service ID "+process.pid);
+res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+res.header("Pragma", "no-cache");
+res.header("Expires", 0);
+res.json({ title: 'Cluster', pid:process.pid });
+});
+
+module.exports = router;
